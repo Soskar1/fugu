24c87967d85ccb0cb6c221cb54928ea99e4e1ba9
oskar <oskar.vistorskis@gmail.com>
2026-08-20 19:22:34 +0300

    getting files recursively

--- src/analyzer/file_system_analyzer.rs
+++ src/analyzer/file_system_analyzer.rs
@@ -3,14 +3,22 @@ use std::path::PathBuf;
 
 pub fn analyze(path: PathBuf) -> u64 {
     let metadata = fs::metadata(&path).unwrap();
-    return metadata.len();
+    let mut size = metadata.len();
+    
+    if path.is_dir() {
+        for entry in fs::read_dir(path).unwrap() {
+            size += analyze(entry.unwrap().path().to_path_buf());
+        }
+    }
+    
+    return size;
 }
 
 #[cfg(test)]
 mod tests {
     use std::fs;
     use rstest::rstest;
-    use tempfile::{NamedTempFile};
+    use tempfile::{tempdir, NamedTempFile};
     use crate::analyzer::file_system_analyzer::analyze;
 
     #[rstest]
@@ -19,12 +27,40 @@ mod tests {
     fn analyze_small_file(#[case] content: &str, #[case] expected: u64) {
         // Arrange
         let file = NamedTempFile::new().unwrap();
-
-        // Act
         fs::write(&file.path(), content).unwrap();
 
-        // Assert
+        // Act
         let result = analyze(file.path().to_path_buf());
+
+        // Assert
         assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn analyze_empty_directory() {
+        // Arrange
+        let directory = tempdir().unwrap();
+
+        // Act
+        let result = analyze(directory.path().to_path_buf());
+
+        // Assert
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn analyze_directory_with_files() {
+        // Arrange
+        let directory = tempdir().unwrap();
+        let _ = NamedTempFile::new_in(directory.path()).unwrap();
+        let file_with_contents = NamedTempFile::new_in(directory.path()).unwrap();
+
+        fs::write(&file_with_contents.path(), "Hello, World!").unwrap();
+
+        // Act
+        let result = analyze(directory.path().to_path_buf());
+
+        // Assert
+        assert_eq!(result, 13);
     }
 }
